72542a128e41aafa065ebab1d51e59a3f5801fa1
Yoselyn Vaquedano <yoselyn.vaquedano@unah.hn>
2021-03-24 18:16:25 -0600

    Contacto del vendedor

--- containers/index.php
+++ containers/index.php
@@ -1,7 +1,21 @@
 <?php 
 include('../layout/navbar.php')
 ?>
-<div>
-  
-    
-</div>
+
+<?php if (isset($_SESSION['correo'])):?> 
+ <div class="container">
+    <div class="row">
+      <div class="col-lg-6"><br>
+      <h4 style="border-bottom: solid 2px gray;"><?php echo $nombre ?></h4><br>
+      <h6 style="border-bottom: solid 2px gray; margin-left: 20px;">Información de Contacto</h6>
+      <div>
+        <p style="border-bottom: solid 2px gray; margin-left: 20px;"> Correo electronico: <?php echo $email ?> </p> 
+        <p style="border-bottom: solid 2px gray; margin-left: 20px;"> Telefono: <?php echo $telefono ?> </p> 
+        <p style="border-bottom: solid 2px gray; margin-left: 20px;"> Dirección: <?php echo $direccion ?> </p> 
+      </div>
+</div> 
+<?php endif;?>
+<?php if (!isset($_SESSION['correo'])):?> 
+<h6 style="border-bottom: solid 2px gray; margin-left: 20px;">Información de Contacto</h6>
+<?php endif;?>
+

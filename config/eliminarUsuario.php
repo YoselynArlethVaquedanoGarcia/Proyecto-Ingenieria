67fcbef4a07800eb2f695069c07cb7186bdf5590
Yoselyn Vaquedano <yoselyn.vaquedano@unah.hn>
2021-04-14 12:54:49 -0600

    Dar de baja a usuarios

---
+++ config/eliminarUsuario.php
@@ -0,0 +1,16 @@
+<?php
+	
+	EliminarCategoria($_GET['idUsuario']);
+echo $_GET['idUsuario'];
+	function EliminarCategoria($idUsuario)
+	{
+        $conexion=mysqli_connect("localhost","Yoselyn","Yoselyn123","proyecto");
+		$consultaCon="DELETE FROM usuario WHERE idUsuario='".$idUsuario."' ";
+		$resultadoCon=mysqli_query($conexion,$consultaCon);
+	}
+?>
+
+<script type="text/javascript">
+	alert("Usuario Eliminado Exitosamente");
+	window.location.href='../html/ltr/usuario.php';
+</script>
